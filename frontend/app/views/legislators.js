f45ab6ee4f2470adbaa7d7477e99bdce17c65e4d
Ben Hudson <bahudso@g.clemson.edu>
2014-11-09 13:59:45 -0500

    Fix warning & errors.

--- frontend/app/views/legislators.js
+++ frontend/app/views/legislators.js
@@ -3,20 +3,20 @@ import Ember from 'ember';
 export default Ember.View.extend({
 	didInsertElement: function() {
 		Ember.$(document).find('div.percentage').each(function() {
-			if ($(this).text() == "100%" || $(this).text() == "90%" || $(this).text() == "80%") {
-				$(this).parent('li').css('border-color', 'green');
-				$(this).css('color', 'green');
-				$(this).parent('li').css('background-color', 'rgba(0,155,0,.5)');
+			if (Ember.$(this).text() === "100%" || Ember.$(this).text() === "90%" || Ember.$(this).text() === "80%") {
+				Ember.$(this).parent('li').css('border-color', 'green');
+				Ember.$(this).css('color', 'green');
+				Ember.$(this).parent('li').css('background-color', 'rgba(0,155,0,.5)');
 			}
-			if ($(this).text() == "70%" || $(this).text() == "60%") {
-				$(this).parent('li').css('border-color', 'orange');
-				$(this).css('color', 'orange');
-				$(this).parent('li').css('background-color', 'rgba(255,165,0,.5)');
+			if (Ember.$(this).text() === "70%" || Ember.$(this).text() === "60%") {
+				Ember.$(this).parent('li').css('border-color', 'orange');
+				Ember.$(this).css('color', 'orange');
+				Ember.$(this).parent('li').css('background-color', 'rgba(255,165,0,.5)');
 			}
-			if ($(this).text() == "30%" || $(this).text() == "20%" || $(this).text() == "10%" || $(this).text() == "50%" || $(this).text() == "40%") {
-				$(this).parent('li').css('border-color', 'red');
-				$(this).css('color', 'red');
-				$(this).parent('li').css('background-color', 'rgba(255,0,0,.5)');
+			if (Ember.$(this).text() === "30%" || Ember.$(this).text() === "20%" || Ember.$(this).text() === "10%" || Ember.$(this).text() === "50%" || Ember.$(this).text() === "40%") {
+				Ember.$(this).parent('li').css('border-color', 'red');
+				Ember.$(this).css('color', 'red');
+				Ember.$(this).parent('li').css('background-color', 'rgba(255,0,0,.5)');
 			}
 		});
 
