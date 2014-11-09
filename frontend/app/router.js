812918485e28dd26a8cba3f0b5cdd9f9ed51ccad
Ben Hudson <bahudso@g.clemson.edu>
2014-11-09 09:14:05 -0500

    Move legislator route into application route and add link for each.

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -7,12 +7,12 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
 	
-	this.route('legislator', {path: 'legislator/:legislator_id'});
 	this.route('application', {path: "/"}, function() {
 		this.resource('bills', function() {
 			this.route('bill', {path: ':bill_id'});
 		});
 		this.route('legislators');
+		this.route('legislator', {path: 'legislator/:legislator_id'});
 	});
 });
 
